699bc50f62e398ba53467f9d37b8f64c0d3022c2
darren <764190362@qq.com>
2017-09-30 14:47:06 +0800

    获取到HttpRequest数据

---
+++ Controllers/DerivedController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreControllersActionsExamples.Controllers
+{
+    public class DerivedController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View("Result", $"This is a derived controller");
+        }
+
+        public ViewResult Headers()
+        {
+            //通过Request属性获取到一些表头信息
+            return View("DictionaryResult", Request.Headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.First()));
+        }
+    }
+}
---
+++ Controllers/PocoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreControllersActionsExamples.Controllers
+{
+    public class PocoController : Controller
+    {
+        // GET: /<controller>/
+        //public IActionResult Index()
+        //{
+        //    return View("Result","This is a POCO controller");
+        //}
+
+        public ViewResult Index()
+        {
+            return new ViewResult() {
+                ViewName="Result",
+
+                //视图页需要拿到一个Model属性，原来这个属性是在ViewData中的
+                //视图页拿到的数据是一个ViewDataDictionary类型
+                //而ViewDatDictionary的形成与元数据提供者ModelMetadataProvider和模型状态ModelStateDictionary有关
+                ViewData= new ViewDataDictionary(
+                        new EmptyModelMetadataProvider(),
+                        new ModelStateDictionary())
+                {
+                    Model = $"This is a POCO controller"
+                }
+            };
+        }
+    }
+}
