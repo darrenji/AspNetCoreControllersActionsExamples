2d0fe429bcadc60043e198ab6b5de158063e261d
darren <764190362@qq.com>
2017-10-01 09:16:39 +0800

    Action参数接受视图数据

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreControllersActionsExamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View("SimpleForm");
+        }
+
+        //使用HttpRequest接收数据
+        //public ViewResult ReceiveForm()
+        //{
+        //    var name = Request.Form["name"];
+        //    var city = Request.Form["city"];
+        //    return View("Result", $"{name} lives in {city}");
+        //}
+
+        
+        //使用方法参数接受数据
+        public ViewResult ReceiveForm(string name, string city)
+        {
+            return View("Result", $"{name} lives in {city}");
+        }
+    }
+}
